e9dc4f463e5712bca5d00922514150765d709ee4
dsoze1138 <dsoze1138@gmail.com>
2019-10-27 11:58:32 -0700

    Update comments

--- 18F252_Timer0.X/main.c
+++ 18F252_Timer0.X/main.c
@@ -49,7 +49,7 @@
  *   [14]LCD_D7      [18]RB7
  *
  * Description:
- *  Initial a PIC18F252 with an external 10MHz crystal for a 40MHz system clock.
+ *  Initialize the PIC18F252 with an external 10MHz crystal for a 40MHz system clock.
  *  Setup a TIMER0 interrupt to assert every 1.6384 milliseconds.
  *  Assign pins to be used for an 8-bit parallel interface to an HD44780 LCD controller.
  *
